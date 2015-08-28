93b3fad0b17a3818942405ec12462afd703d76b1
guiling <zgl88161104@163.com>
2015-08-28 16:13:45 +0800

    fix some issues for adding spark submission code

--- src/com/microsoft/azure/hdinsight/spark/common/SparkBatchSubmission.java
+++ src/com/microsoft/azure/hdinsight/spark/common/SparkBatchSubmission.java
@@ -1,18 +1,15 @@
 package com.microsoft.azure.hdinsight.spark.common;
 
 import org.apache.http.HttpEntity;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.*;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import java.io.IOException;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.google.gson.Gson;
 
 /**
  * Created by guizha on 8/27/2015.
@@ -34,7 +31,7 @@ public class SparkBatchSubmission {
     }
 
     /**
-     *
+     * get all batches spark jobs
      * @param connectUrl : eg http://localhost:8998/batches
      * @return
      * @throws IOException
@@ -54,6 +51,7 @@ public class SparkBatchSubmission {
     }
 
     /**
+     * create batch spark job
      * @param connectUrl : eg http://localhost:8998/batches
      * @param submissonParameter : spark submission parameter
      * @return
@@ -63,11 +61,10 @@ public class SparkBatchSubmission {
         HttpPost httpPost = new HttpPost(connectUrl);
         httpPost.addHeader("Content-Type", "application/json");
 
-        List<BasicNameValuePair> nvps = new ArrayList<BasicNameValuePair>();
         // TODO : Support more parameters
-        nvps.add(new BasicNameValuePair("file", submissonParameter.getFile()));
-        nvps.add(new BasicNameValuePair("className", submissonParameter.getClassName()));
-        httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+        String jsonString = new Gson().toJson(submissonParameter);
+        StringEntity postingString =new StringEntity(jsonString);
+        httpPost.setEntity(postingString);
         CloseableHttpResponse response = httpclient.execute(httpPost);
         try {
             HttpEntity entity = response.getEntity();
@@ -78,7 +75,7 @@ public class SparkBatchSubmission {
     }
 
     /**
-     *
+     * get batch spark job status
      * @param connectUrl : eg http://localhost:8998/batches
      * @param batchId : batch Id
      * @return
@@ -98,6 +95,13 @@ public class SparkBatchSubmission {
         }
     }
 
+    /**
+     * kill batch job
+     * @param connectUrl : eg http://localhost:8998/batches
+     * @param batchId : batch Id
+     * @return
+     * @throws IOException
+     */
     public String killBatchJob(String connectUrl, String batchId)throws IOException {
         CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpDelete httpDelete = new HttpDelete(connectUrl +  "/" + batchId);
@@ -112,4 +116,24 @@ public class SparkBatchSubmission {
         }
     }
 
+    /**
+     * get batch job full log
+     * @param connectUrl : eg http://localhost:8998/batches
+     * @param batchId : batch Id
+     * @return
+     * @throws IOException
+     */
+    public String getBatchJobFullLog(String connectUrl, String batchId)throws IOException{
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(connectUrl + "/" + batchId + "/log");
+        httpGet.addHeader("Content-Type", "application/json");
+
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        try {
+            HttpEntity entity = response.getEntity();
+            return SparkHelper.getResultFromInputStream(entity.getContent());
+        }finally {
+            response.close();
+        }
+    }
 }
--- src/com/microsoft/azure/hdinsight/spark/common/SparkInteractiveSessions.java
+++ src/com/microsoft/azure/hdinsight/spark/common/SparkInteractiveSessions.java
@@ -1,16 +1,15 @@
 package com.microsoft.azure.hdinsight.spark.common;
 
+import com.google.gson.Gson;
 import org.apache.http.HttpEntity;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.*;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import java.io.IOException;
-import java.util.*;
 
 /**
  * Created by guizha on 8/27/2015.
@@ -33,7 +32,7 @@ public class SparkInteractiveSessions {
     }
 
     /**
-     *
+     * get all sessions
      * @param connectUrl : eg http://localhost:8998/sessions
      * @return
      * @throws IOException
@@ -53,6 +52,7 @@ public class SparkInteractiveSessions {
     }
 
     /**
+     * create new session
      * @param connectUrl : eg http://localhost:8998/sessions
      * @param kind : spark or pyspark
      * @return json string result
@@ -62,10 +62,10 @@ public class SparkInteractiveSessions {
         CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost(connectUrl);
         httpPost.addHeader("Content-Type", "application/json");
+        String jsonString = "{\"kind\" : \"" + kind + "\"}";
+        StringEntity postingString =new StringEntity(jsonString);
+        httpPost.setEntity(postingString);
 
-        List<BasicNameValuePair> nvps = new ArrayList <BasicNameValuePair>();
-        nvps.add(new BasicNameValuePair("kind", kind));
-        httpPost.setEntity(new UrlEncodedFormEntity(nvps));
         CloseableHttpResponse response = httpclient.execute(httpPost);
         try {
             HttpEntity entity = response.getEntity();
@@ -98,7 +98,7 @@ public class SparkInteractiveSessions {
     /**
      * @param connectUrl : eg http://localhost:8998/sessions
      * @param sessionId : session id
-     * @param code scala  or python code segment
+     * @param code scala or python code segment
      * @return
      * @throws IOException
      */
@@ -106,10 +106,10 @@ public class SparkInteractiveSessions {
         CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost(connectUrl + "/" + sessionId + "/statements");
         httpPost.addHeader("Content-Type", "application/json");
+        String jsonString = "{\"code\" : \"" + code + "\"}";
+        StringEntity postingString =new StringEntity(jsonString);
+        httpPost.setEntity(postingString);
 
-        List<BasicNameValuePair> nvps = new ArrayList <BasicNameValuePair>();
-        nvps.add(new BasicNameValuePair("code", code));
-        httpPost.setEntity(new UrlEncodedFormEntity(nvps));
         CloseableHttpResponse response = httpclient.execute(httpPost);
         try {
             HttpEntity entity = response.getEntity();
@@ -120,6 +120,7 @@ public class SparkInteractiveSessions {
     }
 
     /**
+     * get job execution status
      * @param connectUrl eg http://localhost:8998/sessions
      * @param sessionId session id
      * @param statementId statement id
@@ -141,6 +142,7 @@ public class SparkInteractiveSessions {
     }
 
     /**
+     * kill session
      * @param connectUrl : eg http://localhost:8998/sessions
      * @param sessionId : session id
      * @return
